6865c0efa8f7dafdd6b9cc026a7917f67ea59151
Ruthvik VIjayakumar <coderuth@outlook.com>
2015-09-27 10:19:17 +0530

    First Commit
    OLA Ride Now Button Integration Tool.

---
+++ app.js
@@ -0,0 +1,61 @@
+
+// create our angular app and inject ngAnimate and ui-router 
+// =============================================================================
+angular.module('formApp', ['ngAnimate', 'ui.router'])
+
+// configuring our routes 
+// =============================================================================
+.config(function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+    
+        // route to show our basic form (/form)
+        .state('form', {
+            url: '/form',
+            templateUrl: 'form.html',
+            controller: 'formController'
+        })
+        
+        // nested states 
+        // each of these sections will have their own view
+        // url will be nested (/form/profile)
+        .state('form.profile', {
+            url: '/profile',
+            templateUrl: 'form-profile.html'
+        })
+        
+        // url will be /form/interests
+        .state('form.theme', {
+            url: '/theme',
+            templateUrl: 'form-theme.html'
+        })
+        
+        // url will be /form/payment
+        .state('form.result', {
+            url: '/result',
+            templateUrl: 'form-result.html'
+        });
+       
+    // catch all route
+    // send users to the form page 
+    $urlRouterProvider.otherwise('/form/profile');
+})
+
+// our controller for the form
+// =============================================================================
+.controller('formController', function($scope) {
+    
+    // we will store all of our form data in this object
+    $scope.formData = {};
+    
+    // function to process the form
+    $scope.processForm = function() {
+        alert('awesome!');  
+    };
+    
+});
+
+
+      
+
+
